3681ed07e99bc6069334fc410ae1c5cf86327f2d
eddielee <eddie27lee@gmail.com>
2019-02-17 16:43:32 -0500

    Filling Spinner

---
+++ app/src/main/java/com/example/spacetrader/Difficulty.java
@@ -0,0 +1,11 @@
+package com.example.spacetrader;
+
+public enum Difficulty {
+    BASIC,
+    NORMAL,
+    CHALLENGING,
+    DEMANDING,
+    HARD,
+    BRUTAL,
+    IMPOSSIBLE;
+}
---
+++ app/src/main/java/com/example/spacetrader/EditDifficulty.java
@@ -0,0 +1,20 @@
+package com.example.spacetrader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class EditDifficulty extends AppCompatActivity {
+    private Spinner difficultySpinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        difficultySpinner = findViewById(R.id.difficulty_spinner);
+        ArrayAdapter<Difficulty> standingDifficulty = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item);
+        standingDifficulty.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        difficultySpinner.setAdapter(standingDifficulty);
+    }
+}
