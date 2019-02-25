1885e5a66e69abd518785e2fcdb2324e69af8cd0
Nyaire Najieb <nyaire.najieb@gatech.edu>
2019-02-24 20:22:10 -0500

    implemented the tech level enum

--- app/src/main/java/com/example/spacetrader/models/TechLevel.java
+++ app/src/main/java/com/example/spacetrader/models/TechLevel.java
@@ -1,4 +1,20 @@
 package com.example.spacetrader.models;
 
 public enum TechLevel {
+    PA (0, "PRE-AGRICULTURE"),
+    AG (1, "AGRICULTURE"),
+    MD (2, "MEDIEVAL"),
+    RN (3, "RENAISSANCE"),
+    EI (4, "EARLY INDUSTRIAL"),
+    IN (5, "INDUSTRIAL"),
+    PI (6, "POST INDUSTRIAL"),
+    HT (7, "HI-TECH");
+
+    private int level;
+    private String name;
+
+    TechLevel (int level, String name) {
+        this.level = level;
+        this.name = name;
+    }
 }
