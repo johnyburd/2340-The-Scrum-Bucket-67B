bc95b86a1aa82a7521cf011982305f1adc2f7c77
eddielee <eddie27lee@gmail.com>
2019-03-06 17:02:35 -0500

    Added base prices to Market

--- app/src/main/java/com/example/spacetrader/entity/Market.java
+++ app/src/main/java/com/example/spacetrader/entity/Market.java
@@ -2,4 +2,41 @@ package com.example.spacetrader.entity;
 
 public class Market {
 
+    public Market() {
+
+    }
+
+    private int BasePrice(Good good) {
+        if (good.equals(Good.WATER)) {
+            return 30;
+        }
+        else if (good.equals(Good.FURS)) {
+            return 250;
+        }
+        else if (good.equals(Good.FOOD)) {
+            return 100;
+        }
+        else if (good.equals(Good.ORE)) {
+            return 350;
+        }
+        else if (good.equals(Good.GAMES)) {
+            return 250;
+        }
+        else if (good.equals(Good.FIREARMS)) {
+            return 1250;
+        }
+        else if (good.equals(Good.MEDICINE)) {
+            return 650;
+        }
+        else if (good.equals(Good.MACHINES)) {
+            return 900;
+        }
+        else if (good.equals(Good.NARCOTICS)) {
+            return 3500;
+        }
+        else if (good.equals(Good.ROBOTS)) {
+            return 5000;
+        }
+        else return -1;
+    }
 }
