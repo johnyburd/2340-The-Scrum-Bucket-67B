ed54eca947598362e42cdb048efdf2caf5e82845
iwang2 <wangida02@gmail.com>
2019-03-11 01:27:09 -0400

    created pirate/police encounter xml screens, attempted to start on a tab layout for buy/sell/yard/warp screens

--- app/src/main/java/com/example/spacetrader/views/MainActivity.java
+++ app/src/main/java/com/example/spacetrader/views/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
+        setContentView(R.layout.create_player);
 
         viewModel = ViewModelProviders.of(this).get(PlayerViewModel.class);
 
